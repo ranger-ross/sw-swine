783b0e0acaadede64b10a5b938a38d647dc4a871
Ross Sullivan <rosssullivan101@gmail.com>
2020-07-15 21:05:56 -0500

    added bootstrap and created navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sw-swine';
+  title = 'Home';
+
+  constructor(private router: Router) {
+  }
+
+  // get _title(): string {
+  //   return this.router.
+  // }
+
 }
